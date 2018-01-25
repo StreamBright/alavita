275851d5d1cba832f626889d00f4ef6c148efd34
Istvan Szukacs <istvan@streambrightdata.com>
2018-01-25 08:34:29 +0100

    Clean up

--- src/alavita/util.clj
+++ src/alavita/util.clj
@@ -2,28 +2,28 @@
 
 ;; See LICENSE
 
-(ns ^{	:doc "alavita :: util"
-				:author "Istvan Szukacs"			}
-	alavita.util
-	(:import
-		[java.util	UUID							]
-		[java.text	SimpleDateFormat	]
-		[java.time	Instant						]
-		[java.util	Date							] )
-	)
+(ns ^{  :doc "alavita :: util"
+        :author "Istvan Szukacs"      }
+  alavita.util
+  (:import
+    [java.util  UUID              ]
+    [java.text  SimpleDateFormat  ]
+    [java.time  Instant           ]
+    [java.util  Date              ] )
+  )
 
 (defn get-utc-string-of-now
-	"Return a string of UTC now"
-	[]
-	(.format
-		(SimpleDateFormat. "yyyy_MM_dd_HH_mm")
-		(Date/from (Instant/now))))
+  "Return a string of UTC now"
+  []
+  (.format
+    (SimpleDateFormat. "yyyy_MM_dd_HH_mm")
+    (Date/from (Instant/now))))
 
 (defn uuid
-	"Returns a new java.util.UUID as string"
-	[]
-	(str (UUID/randomUUID)))
+  "Returns a new java.util.UUID as string"
+  []
+  (str (UUID/randomUUID)))
 
 (defn exit
-	[n]
-	(System/exit n))
+  [n]
+  (System/exit n))
